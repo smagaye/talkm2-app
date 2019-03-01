792f63faa597950eb3d7c5a7a708c2c2fca0904d
Sma Gaye <sma.gaye94@gmail.com>
2019-03-01 22:16:16 +0000

    package definition fixed! - Template engine added - navigation rule defined

--- developpement/src/main/java/com/smag/talkm2/controller/Homepage.java
+++ developpement/src/main/java/com/smag/talkm2/controller/Homepage.java
@@ -1,14 +1,31 @@
 package com.smag.talkm2.controller;
 
 
+import com.smag.talkm2.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Homepage {
-    @GetMapping("/")
-    private String index(){
-        return "index.html";
+    @RequestMapping ("/")
+    private String index(Model model){
+        return "index.xhtml";
     }
+
+    @RequestMapping ("/sign_in")
+    private String signIn(Model model){
+        return "workspace/forms/sign_in.xhtml";
+    }
+
+    @RequestMapping ("/login")
+    private String login(Model model){
+        return "workspace/forms/login.xhtml";
+    }
+
 
 }
